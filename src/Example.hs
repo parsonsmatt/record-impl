2e1208a7007c1963c6129001a6f519cf7f293036
parsonsmatt <parsonsmatt@gmail.com>
2022-03-14 14:42:20 -0600

    wooo

--- src/Example.hs
+++ src/Example.hs
@@ -1,5 +1,24 @@
+{-# language QuasiQuotes #-}
+{-# language TemplateHaskell #-}
+
 module Example where
 
-import Impl
+import Impl.TH
 
--- $> :browse
+impl ''User [d|
+
+    applyName :: (String -> String) -> String
+    applyName f =
+        f self.name
+
+    sayName :: _
+    sayName =
+        putStrLn self.name
+
+    |]
+
+runExample :: IO ()
+runExample = do
+    let user = User { userName = "Matt", userBirthYear = 33 }
+
+    user.sayName
--- src/Impl/TH.hs
+++ src/Impl/TH.hs
@@ -1,4 +1,4 @@
-{-# language TypeApplications, RankNTypes, TypeFamilyDependencies, TypeFamilies, GADTs, ScopedTypeVariables, UndecidableInstances, ImpredicativeTypes, OverloadedRecordDot, QuasiQuotes, NoMonomorphismRestriction, DataKinds, FlexibleContexts , TemplateHaskell #-}
+{-# language LambdaCase, EmptyCase, TypeApplications, RankNTypes, TypeFamilyDependencies, TypeFamilies, GADTs, ScopedTypeVariables, UndecidableInstances, ImpredicativeTypes, OverloadedRecordDot, QuasiQuotes, NoMonomorphismRestriction, DataKinds, FlexibleContexts , TemplateHaskell #-}
 
 {-# language RankNTypes, FlexibleInstances, MultiParamTypeClasses, FunctionalDependencies, ExistentialQuantification, ExplicitForAll, ImpredicativeTypes, RankNTypes #-}
 
@@ -6,34 +6,25 @@
 
 module Impl.TH where
 
-import Prelude
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Prelude hiding (exp)
 import Data.Time
-import Control.Monad.IO.Class
 import GHC.Records
 import Language.Haskell.TH
-import Language.Haskell.TH.Syntax
-import Language.Haskell.TH.Quote
+-- import Language.Haskell.TH.Syntax
 
 giveIdentity :: IO (forall a . Show a => a -> String)
 giveIdentity = pure show
 
 data User = User
-    { name :: String
-    , birthYear :: Integer
+    { userName :: String
+    , userBirthYear :: Integer
     }
     deriving Show
 
--- $> go
-
-go :: IO ()
-go = do
-    let user = User { name = "Matt", birthYear = 1988}
-    user.myPrint 'a'
-    user.myPrint 3
-    print =<< user.age
-
 instance HasField "setName" User (String -> User) where
-    getField self str = self { name = str }
+    getField self str = self { userName = str }
 
 instance
     ( Show a
@@ -43,16 +34,13 @@ instance
     HasField "myPrint" User (a -> IO ())
   where
     getField self a = do
-        putStrLn $ concat [self.name, " says: ", show a]
+        putStrLn $ concat [self.userName, " says: ", show a]
 
 instance HasField "age" User (IO Integer) where
     getField self = do
         (y, _, _) <- toGregorian . utctDay <$> getCurrentTime
-        pure $ y - self.birthYear
+        pure $ y - self.userBirthYear
 
-
-x :: Quote w => Code w Integer
-x = [|| 1 + 2 ||]
 
 -- What are we going for? Well, let's make functions on records.
 
@@ -124,11 +112,184 @@ impl typ decsQ = do
     decs <-
         [d|
             instance  HasField "explode" $(conT typ) (IO ()) where
-                getField self = do
+                getField _self = do
                     putStrLn "oh no"
             |]
-    realDecs <- decsQ
-    traverse (reportError . show)  $ realDecs
+    selfName <- newName "self"
+    realDecs <- map (fixDecs typ selfName) <$> decsQ
+    _ <- traverse (reportWarning . show)  $ realDecs
     pure decs
---
--- -- $> :t impl
+
+collectDecs :: [Dec] -> Map Name [Dec]
+collectDecs [] = mempty
+collectDecs (d : ds) = Map.unionWith mappend go $ collectDecs ds
+  where
+    go =
+        case d of
+            ValD pat _ _ ->
+                Map.singleton (getName pat) [d]
+            FunD name _ ->
+                Map.singleton name [d]
+            SigD name _ ->
+                Map.singleton name [d]
+            KiSigD name _ ->
+                Map.singleton name [d]
+            _ ->
+                error $ "unsupported dec: " <> show d
+    getName p = case p of
+        VarP name ->
+            name
+        ConP {} ->
+            error "constructor not supported"
+        LitP {} ->
+            no
+        TupP {} -> no
+        UnboxedTupP {} -> no
+        InfixP {} -> no
+        UnboxedSumP {} -> no
+        UInfixP {} -> no
+        ParensP {} -> no
+        TildeP {} -> no
+        BangP {} -> no
+        _ -> no
+
+      where
+        no = error $ "unsupported pattern: " <> show p
+
+
+
+fixDecs :: Name -> Name -> Dec -> Dec
+fixDecs typeName selfName d = go
+  where
+
+    replaceSelf :: Exp -> Exp
+    replaceSelf x = case x of
+        UnboundVarE nm
+            | nm == mkName "self" ->
+                error "asdfasdfdasdf"
+        VarE _ ->
+            x
+        ConE _ ->
+            x
+        _ ->
+            x
+
+    mkLambda :: Exp -> Exp
+    mkLambda = LamE [SigP (VarP selfName) (ConT typeName)]
+
+    addSelfGuardExps :: (Guard, Exp) -> (Guard, Exp)
+    addSelfGuardExps (grd, exp) = (newGrd, newExp)
+      where
+        newExp =
+            case exp of
+                VarE {} ->
+                    exp
+                ConE {} ->
+                    exp
+                LitE {} ->
+                    exp
+                AppE e0 e1 ->
+                    mkLambda $ AppE (replaceSelf e0) (replaceSelf e1)
+                AppTypeE e0 t ->
+                    AppTypeE (mkLambda e0) t
+                InfixE e0 e1 e2 ->
+                    mkLambda $ InfixE (replaceSelf <$> e0) (replaceSelf e1) (replaceSelf <$> e2)
+                UInfixE e0 e1 e2 ->
+                    mkLambda $ UInfixE (replaceSelf e0) (replaceSelf e1) (replaceSelf e2)
+                ParensE e ->
+                    ParensE $ mkLambda $ replaceSelf e
+                LamE a b ->
+                    mkLambda $ LamE a (replaceSelf b)
+                LamCaseE matches ->
+                    mkLambda $ LamCaseE (map fixMatch matches)
+                TupE mexps ->
+                    mkLambda $ TupE (map (fmap replaceSelf) mexps)
+                UnboxedTupE mexps ->
+                    mkLambda $ UnboxedTupE (map (fmap replaceSelf) mexps)
+                UnboxedSumE exp i j ->
+                    mkLambda $ UnboxedSumE (replaceSelf exp) i j
+                CondE a b c ->
+                    mkLambda $ CondE (replaceSelf a) (replaceSelf b) (replaceSelf c)
+                MultiIfE xs ->
+                    mkLambda $ MultiIfE (map _f xs)
+        newGrd =
+            case grd of
+                NormalG exp' ->
+                    NormalG $ replaceSelf exp'
+
+    fixMatch :: Match -> Match
+    fixMatch = \case
+
+    addSelfLambda :: Body -> Body
+    addSelfLambda bdy =
+        case bdy of
+            GuardedB guard'exps ->
+                GuardedB $ map addSelfGuardExps guard'exps
+            NormalB exp ->
+                NormalB $ LamE [SigP (VarP selfName) (ConT typeName)] exp
+
+
+    addSelfClause :: Clause -> Clause
+    addSelfClause (Clause patterns body decs) =
+        Clause ((VarP selfName) : patterns) (addSelfLambda body) (map replaceSelfDecs decs)
+
+    replaceSelfDecs :: Dec -> Dec
+    replaceSelfDecs = \case
+
+
+    addSelfType :: Type -> Type
+    addSelfType t =
+        ArrowT `AppT` ConT typeName `AppT` t
+
+    go =
+        case d of
+            ValD pat body extras ->
+                ValD pat (addSelfLambda body) extras
+            FunD name clauses ->
+                FunD name (map addSelfClause clauses)
+            SigD name typ ->
+                SigD name (addSelfType typ)
+            KiSigD name typ ->
+                KiSigD name (addSelfType typ)
+            ForeignD _ ->
+                error "foreign unsupported"
+            InfixD fixity name ->
+                InfixD fixity name
+
+            -- it would be cool to support these
+            PragmaD {} ->
+                error "Pragma unsupported"
+            DataFamilyD {} ->
+                error "data family unsupported"
+            DataInstD {} ->
+                error "Data instance unsupported"
+            NewtypeInstD {} ->
+                error "newtype instance unsupported"
+            TySynInstD {} ->
+                error "type family instance unsupported"
+            OpenTypeFamilyD {} ->
+                error "type family declaration unsupported"
+            ClosedTypeFamilyD {} ->
+                error "close tyfam unsupported"
+            RoleAnnotD {} ->
+                error "role annotations unsupported"
+            StandaloneDerivD {} ->
+                error "standalone deriivng unsupported"
+            DefaultSigD {} ->
+                error "default unsupported"
+            PatSynD {} ->
+                error "pattern synonym unsupported"
+            PatSynSigD {} ->
+                error "pattern synonym unsupported"
+            ImplicitParamBindD {} ->
+                error "implicit param unsupported"
+            DataD {} ->
+                error "Data declarations unsupported"
+            NewtypeD {} ->
+                error "newtype unsupported"
+            TySynD {} ->
+                error "type synonym unsupported"
+            ClassD {} ->
+                error "classes unsupported"
+            InstanceD {} ->
+                error "instances unsupported"
